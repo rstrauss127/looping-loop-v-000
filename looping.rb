02ff8a181e1442493c5dc89473ac4e1a0efd928e
Rebecca Strauss <rstrauss127@gmail.com>
2018-01-11 23:52:13 +0000

    Done.

--- looping.rb
+++ looping.rb
@@ -1,5 +1,13 @@
 def looping
-	#your code here
+  counter = 1
+  loop do
+    if counter == 0
+      break
+    else
+      puts "Wingardium Leviosa"
+      counter += 1
+    end	#your code here
+  end
 end
-
+looping
 #call your method here
